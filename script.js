92b64d363341273cf07c1e08dc7c75ab64358bef
Dylan Best <best.dylan15@gmail.com>
2020-07-26 13:38:14 -0400

    started formatting current weather results

--- script.js
+++ script.js
@@ -1,14 +1,16 @@
 // variables
-var cityName = $("#city-name");
+var citySearch = $("#city-name");
+var currentWeatherResults = $("div.current-weather-results");
 
 var url = "https://api.openweathermap.org/data/2.5/weather?";
 var apiKey = "7c5da79212fcccfaa4134fd2a597f8b6";
 
 // search button click event
 $("#search").on("click", function () {
+    currentWeatherResults.empty();
 
     // create query url with city name
-    var queryURL = `${url}q=${cityName.val()}&appid=${apiKey}`;
+    var queryURL = `${url}q=${citySearch.val()}&appid=${apiKey}`;
 
     // get request for current weather data
     $.ajax({
@@ -17,5 +19,16 @@ $("#search").on("click", function () {
     }).then(function(response) {
 
         console.log(response);
+
+        // create current weather data
+        var cityName = $("<h2>").text(response.name);
+        var temp = $("<p>").text(`Temperature: ${response.main.temp}`);
+        var humidity = $("<p>").text(`Humidity: ${response.main.humidity}%`);
+        var wind = $("<p>").text(`Wind Speed: ${response.wind.speed} MPH`);
+        var uvIndex = $("<p>").text(`UV Index: `);
+
+        // append to empty div
+        currentWeatherResults.append(cityName, temp, humidity, wind, uvIndex);
+
     });
 })
