3500b64f56da7bc5df0f900df839415f39ee97e7
Dylan Best <best.dylan15@gmail.com>
2020-07-28 09:20:59 -0400

    fixed syntax on functions

--- script.js
+++ script.js
@@ -6,6 +6,34 @@ var forecastResults = $("div.forecast-results");
 var url = "https://api.openweathermap.org/data/2.5/weather?";
 var apiKey = "7c5da79212fcccfaa4134fd2a597f8b6";
 
+function getCurrentWeather() {
+
+    // create query url with city name
+    var queryURL = `${url}q=${citySearch.val()}&units=imperial&appid=${apiKey}`;
+
+    // get request for current weather data
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+    }).then(function (response) {
+
+        console.log(response);
+
+        // create current weather data
+        var cityName = $("<h2>").text(response.name);
+        var temp = $("<p>").text(`Temperature: ${response.main.temp} °F`);
+        var humidity = $("<p>").text(`Humidity: ${response.main.humidity}%`);
+        var wind = $("<p>").text(`Wind Speed: ${response.wind.speed} MPH`);
+        var uvIndex = $("<p>").text(`UV Index: `);
+
+        // append to empty div
+        currentWeatherResults.append(cityName, temp, humidity, wind, uvIndex);
+
+        // call get forecast function using coordinates
+        getForecast(response.coord.lat, response.coord.lon);
+    });
+};
+
 function getForecast(lat, lon) {
 
     // create second query url with coordinates
@@ -37,37 +65,17 @@ function getForecast(lat, lon) {
             cardBody.append(temp, humidity);
             card.append(cardBody);
             forecastResults.append(card);
-        }
-    })
-}
+        };
+    });
+};
 
 // search button click event
 $("#search").on("click", function () {
+
+    // clear results
     currentWeatherResults.empty();
     forecastResults.empty();
 
-    // create query url with city name
-    var queryURL = `${url}q=${citySearch.val()}&units=imperial&appid=${apiKey}`;
-
-    // get request for current weather data
-    $.ajax({
-        url: queryURL,
-        method: "GET"
-    }).then(function (response) {
-
-        console.log(response);
-
-        // create current weather data
-        var cityName = $("<h2>").text(response.name);
-        var temp = $("<p>").text(`Temperature: ${response.main.temp} °F`);
-        var humidity = $("<p>").text(`Humidity: ${response.main.humidity}%`);
-        var wind = $("<p>").text(`Wind Speed: ${response.wind.speed} MPH`);
-        var uvIndex = $("<p>").text(`UV Index: `);
-
-        // append to empty div
-        currentWeatherResults.append(cityName, temp, humidity, wind, uvIndex);
-
-        // call get forecast function using coordinates
-        getForecast(response.coord.lat, response.coord.lon);
-    });
-})
+    // call get current weather function
+    getCurrentWeather();
+});
