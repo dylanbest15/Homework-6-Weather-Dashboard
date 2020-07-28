7b96e0b52f6342bc6d5fea523d8ed644ebe3eab6
Dylan Best <best.dylan15@gmail.com>
2020-07-28 09:14:53 -0400

    created forecast cards format

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 // variables
 var citySearch = $("#city-name");
 var currentWeatherResults = $("div.current-weather-results");
+var forecastResults = $("div.forecast-results");
 
 var url = "https://api.openweathermap.org/data/2.5/weather?";
 var apiKey = "7c5da79212fcccfaa4134fd2a597f8b6";
@@ -8,41 +9,57 @@ var apiKey = "7c5da79212fcccfaa4134fd2a597f8b6";
 function getForecast(lat, lon) {
 
     // create second query url with coordinates
-    var secondURL = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&appid=${apiKey}`;
+    var secondURL = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&units=imperial&appid=${apiKey}`;
 
     $.ajax({
         url: secondURL,
         method: "GET"
-    }).then(function(forecast) {
+    }).then(function (forecast) {
 
         console.log(forecast);
 
         for (var i = 0; i < 5; i++) {
 
-            // create forecast card
-        }
-    });
+            // create forecast cards
+            var card = $("<div>").addClass("card");
+            var cardBody = $("<div>").addClass("card-body");
 
+            // create current date using memento
+            // create icon
+
+            // create temperature
+            var temp = $("<p>").text(`Temp: ${forecast.daily[i + 1].temp.day} °F`);
+
+            // create humidity
+            var humidity = $("<p>").text(`Humidity: ${forecast.daily[i + 1].humidity}%`)
+
+            // append to empty div
+            cardBody.append(temp, humidity);
+            card.append(cardBody);
+            forecastResults.append(card);
+        }
+    })
 }
 
 // search button click event
 $("#search").on("click", function () {
     currentWeatherResults.empty();
+    forecastResults.empty();
 
     // create query url with city name
-    var queryURL = `${url}q=${citySearch.val()}&appid=${apiKey}`;
+    var queryURL = `${url}q=${citySearch.val()}&units=imperial&appid=${apiKey}`;
 
     // get request for current weather data
     $.ajax({
         url: queryURL,
         method: "GET"
-    }).then(function(response) {
+    }).then(function (response) {
 
         console.log(response);
 
         // create current weather data
         var cityName = $("<h2>").text(response.name);
-        var temp = $("<p>").text(`Temperature: ${response.main.temp}`);
+        var temp = $("<p>").text(`Temperature: ${response.main.temp} °F`);
         var humidity = $("<p>").text(`Humidity: ${response.main.humidity}%`);
         var wind = $("<p>").text(`Wind Speed: ${response.wind.speed} MPH`);
         var uvIndex = $("<p>").text(`UV Index: `);
